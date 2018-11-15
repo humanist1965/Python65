72e25b1d6bb7ae43a33c4ec127639d32c36420e7
mkersh <kershaw.mark@gmail.com>
2018-11-15 07:57:54 +0000

    update

--- NextVideo/app.py
+++ NextVideo/app.py
@@ -25,25 +25,25 @@ def getNextEpisode(seriesID):
     nv = NextVideo("mark")
     return json.dumps(nv.getNextEpisode(seriesID))
 
-@app.route("/Series/<seriesID>/Inc")
+@app.route("/Series/<seriesID>/Inc", methods=['GET', 'POST'])
 def incEpisodeNum(seriesID):
     nv = NextVideo("mark")
     nv.incEpisodeNum(seriesID)
     return json.dumps(nv.getNextEpisode(seriesID))
 
-@app.route("/Series/<seriesID>/Dec")
+@app.route("/Series/<seriesID>/Dec", methods=['GET', 'POST'])
 def decEpisodeNum(seriesID):
     nv = NextVideo("mark")
     nv.decEpisodeNum(seriesID)
     return json.dumps(nv.getNextEpisode(seriesID))
 
-@app.route("/Series/<seriesID>/IncSeason")
+@app.route("/Series/<seriesID>/IncSeason", methods=['GET', 'POST'])
 def incSeasonNum(seriesID):
     nv = NextVideo("mark")
     nv.incSeasonNum(seriesID)
     return json.dumps(nv.getNextEpisode(seriesID))
 
-@app.route("/Series/<seriesID>/DecSeason")
+@app.route("/Series/<seriesID>/DecSeason", methods=['GET', 'POST'])
 def decSeasonNum(seriesID):
     nv = NextVideo("mark")
     nv.decSeasonNum(seriesID)
