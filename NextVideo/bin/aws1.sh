39d3b5e6d5376a77feeba38e77ec9efdbc29fa06
humanist1965 <humanist1965@gmail.com>
2018-12-10 08:21:19 +0000

    update

--- NextVideo/bin/aws1.sh
+++ NextVideo/bin/aws1.sh
@@ -2,4 +2,7 @@
 
 # script to start a ssh session on my Amazon EC2 instance that I run the App off
 cd ~/AWS/KEYPAIRS
-ssh -i "MK_KEYPAIR1.pem" ubuntu@ec2-18-130-245-71.eu-west-2.compute.amazonaws.com
+# ssh -i "MK_KEYPAIR1.pem" ubuntu@ec2-18-130-245-71.eu-west-2.compute.amazonaws.com
+ssh -i "MK_KEYPAIR1.pem" ubuntu@ec2-52-56-214-201.eu-west-2.compute.amazonaws.com
+
+
