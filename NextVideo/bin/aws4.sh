28b1713184d946c5e8fc99a79a46ce953bd6874e
humanist1965 <humanist1965@gmail.com>
2022-02-19 19:53:12 +0000

    update

--- NextVideo/bin/aws4.sh
+++ NextVideo/bin/aws4.sh
@@ -3,7 +3,7 @@
 # script to start a ssh session on my Amazon EC2 instance that I run the App off
 cd ~/AWS/KEYPAIRS
 
-ssh -i "MK65_4.pem" ubuntu@ec2-54-217-28-69.eu-west-1.compute.amazonaws.com
+ssh -i "MK65_3.pem" ubuntu@ec2-54-217-28-69.eu-west-1.compute.amazonaws.com
 
 
 
